7f5d5af157e3e6df1e7fe8a7d11485326f3f18eb
Saurabh Kacholiya <kacholiyasaurabh@gmail.com>
2019-02-27 17:04:35 +0530

    add test cases

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 'use strict'
 
+function testParser(string) {
+    console.log(string, null_parser(string))
+}
+
+testParser("null ")
+testParser(" null ")
+testParser("12null ")
+testParser("null123")
+
 function null_parser(data) {
-    if(data.subString(0,4) === "null"){
-        return [null,data.subString(4)]
+    if(data.substring(0,4) === "null"){
+        return [null,data.substring(4)]
     }else{
         return null
     }
