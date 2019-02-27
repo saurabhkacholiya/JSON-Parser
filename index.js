a804da4082dc4cf5727dac93be57b54eced1e67e
Saurabh Kacholiya <kacholiyasaurabh@gmail.com>
2019-02-27 17:18:33 +0530

    add a boolean_parser

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 'use strict'
 
 function testParser(string) {
-    console.log(string, null_parser(string))
+    console.log(string, boolean_parser(string))
 }
 
-testParser("null ")
-testParser(" null ")
-testParser("12null ")
-testParser("null123")
+testParser("true ")
+testParser(" true ")
+testParser("false")
+testParser("_trueFalse")
+testParser("")
+
 
 function null_parser(data) {
     if(data.substring(0,4) === "null"){
         return [null,data.substring(4)]
     }else{
         return null
+    }
+}
+
+function boolean_parser(data){
+    if(data.substring(0,4) === "true"){
+        return [true,data.substring(4)]
+    }else if(data.substring(0,5) === "false"){
+        return [false,data.substring(5)]
+    }else{
+        return null;
     }
 }
