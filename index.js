230790538d2dd89913089a68955ab58db1988a80
Saurabh Kacholiya <kacholiyasaurabh@gmail.com>
2019-03-05 13:16:28 +0530

    add value_parser

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function testParser(string) {
     try {
         correct = JSON.parse(string)
     } catch (error) {
-        
+
     }
     console.log(string, number_parser(string), correct)
 }
@@ -40,25 +40,25 @@ function string_parser(data) {
         let parsedString = '';
         let length = 0;
         for (let i = 1; i < data.length; i++) {
-            length+=1;
-            if (data[i] === '"' && data[i-1] !== '\\') {
+            length += 1;
+            if (data[i] === '"' && data[i - 1] !== '\\') {
                 break;
             }
-            if(data[i] === '\\'){
+            if (data[i] === '\\') {
                 continue;
             }
-            if(data[i-1] === '\\'){
-                switch(data[i]){
-                    case "t" : parsedString+="\t"
-                    break;
-                    case "n" : parsedString+="\n"
-                    break;
-                    case "b" : parsedString += "\b"
-                    break;
-                    case "r" : parsedString += "\r"
-                    break;
+            if (data[i - 1] === '\\') {
+                switch (data[i]) {
+                    case "t": parsedString += "\t"
+                        break;
+                    case "n": parsedString += "\n"
+                        break;
+                    case "b": parsedString += "\b"
+                        break;
+                    case "r": parsedString += "\r"
+                        break;
                 }
-            }else{
+            } else {
                 parsedString += data[i]
             }
         }
@@ -68,12 +68,17 @@ function string_parser(data) {
     }
 }
 
-function number_parser(data){
+function number_parser(data) {
     let regex = /^-?(?:[1-9][0-9]*|0)(?:.[0-9]+)?(?:[eE][+-]?[0-9]+)?/
     let match = data.match(regex)
-    if(match){ 
-        console.log("match",match)
-        return [Number(match[0]),data.slice(match[0].length)]
+    if (match) {
+        console.log("match", match)
+        return [Number(match[0]), data.slice(match[0].length)]
     }
     return null;
+}
+
+function value_parser(data) {
+    return (string_parser(data) || number_parser(data) || null_parser(data) ||
+        boolean_parser(data))
 }
