8e516aeb0d6470098c314f8baf19e9750e1e3b9a
Saurabh Kacholiya <kacholiyasaurabh@gmail.com>
2019-05-16 13:43:01 +0530

    add more practice code

--- prac.js
+++ prac.js
@@ -29,6 +29,8 @@
 // }
 
 
+//=====================================================
+
 // let obj = {property : 1, method: () => {} }
 
 // console.log('Object.entries',Object.entries(obj))
@@ -38,11 +40,18 @@
 // console.log('Object.values',Object.values(obj))
 
 
+//=====================================================
+
+
+//=============================================================
+
 // let entriesObject = Object.entries(obj)
 
 // console.log('entriesObject[0][0] is ', entriesObject[0][0])
 
 // console.log('entriesObject[0][1] is ', entriesObject[0][1])
+
+//=============================================================
 
 
 // let object = {
@@ -76,14 +85,133 @@
 // console.log(matches)
 
 
-const string = 'black*raven lime*parrot white*seagull'
-const regex = /(?<color>.*?)\*(?<bird>[a-z0-9]+)/
-let match
-while(match=regex.exec(string)){
-    let value = match[0]
-    let index = match.index
-    let input = match.input
-    console.log(`${value} at ${index} with ${input}`)
-    console.log(match.groups.color)
-    console.log(match.groups.bird)
-}
+// const string = 'black*raven lime*parrot white*seagull'
+// const regex = /(?<color>.*?)\*(?<bird>[a-z0-9]+)/
+// let match
+// while(match=regex.exec(string)){
+//     let value = match[0]
+//     let index = match.index
+//     let input = match.input
+//     console.log(`${value} at ${index} with ${input}`)
+//     console.log(match.groups.color)
+//     console.log(match.groups.bird)
+// }
+
+
+
+// function add (a,b,c,d="hi"){
+//     console.log(arguments)
+// }
+
+// add(7,["a","c"],{})
+
+// function returnString(){
+//     return "returnString"
+// }
+
+// function display(text,returnString,abc){
+//     console.log(text)
+//     returnString()
+//     console.log(abc)
+// }
+
+// display('saurabh',returnString,returnString())
+
+
+// function Classic() {
+//     let b = function () {
+//         console.log("hello i am in classic")
+//         console.log(this)
+//     }
+//     b()
+// }
+
+// let object = new Classic()
+
+// class Saurabh {
+
+//    function() {
+//         console.log("Hello i am in c")
+//         console.log(this)   
+//     }
+// }
+
+// let saurabh = new Saurabh()
+
+
+
+// function Hello(name) {
+
+// }
+
+// console.log(typeof(Hello.prototype))
+
+// let saurabh = {
+//     surname:"kacholiya",
+//     age : function(){
+//         console.log('hehehe cant say')
+//     }
+// }
+
+// console.log(saurabh.__proto__.surname)
+
+
+// let a = {
+//     b : function(){
+//         console.log('hello')
+//     }
+// }
+
+// let c = a 
+
+// c.b()
+
+// let p = new p;
+
+// let instance = new Number()
+// instance.prop = 123;
+// instance.meth = function(){}
+
+// console.log(instance.__proto__)
+
+
+
+// let array = new Array()
+// Array.prototype.kuchbhi = 234
+// console.log(array.kuchbhi)
+
+
+
+// function test(){
+//     var a = 1 ;
+    
+//     for(var a = 1; a< 5 ; a++){}
+
+//     console.log(a)
+//   }
+  
+  
+// test()  
+
+
+
+// const newString = () => 'Hello there'
+
+// newString() 
+
+// console.log(newString())
+
+
+
+
+// let array = [1,2,3,4,5,6,7,8,9]
+
+// let newArray = array.slice()
+
+// newArray.splice(2,1)
+
+// console.log(newArray)
+
+// console.log(array);
+
+
