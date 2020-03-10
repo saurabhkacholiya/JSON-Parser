08ee5399faecd5b4a032d828d29244d9ab9398c8
Saurabh Ramesh Kacholiya <39425799+saurabhkacholiya@users.noreply.github.com>
2020-03-10 14:35:03 +0530

    How internally createStore works

---
+++ CreateStoreWorks.js
@@ -0,0 +1,26 @@
+const createStore = (reducer) => {
+  let state;
+  let listeners = []
+
+  const getState = () => state
+
+  const dispatch = (action) => {
+    state = reducer(state,action)
+    listeners.forEach(listener => listener())
+  }
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners.filter(l => l !== listener)
+    }
+  }
+
+  dispatch({}) //state initialize 
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  }
+}
